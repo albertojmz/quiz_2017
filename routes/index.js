94d164384144d163489224acca81a31bfa49fd02
albertojmz <albertito1696@gmail.com>
2017-05-19 11:51:34 +0200

    solucion

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,6 @@ router.get('/author', function(req, res, next) {
     res.render('author');
 });
 
-<<<<<<< HEAD
 
 // Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);
@@ -32,10 +31,10 @@ router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 
 
-=======
+
 // Pagina de ayuda
 router.get('/help', function(req, res, next) {
     res.render('help');
 });
->>>>>>> practica51
+
 module.exports = router;
